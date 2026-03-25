32778a83ec15c69b0e069808090072f5d36020b2
farehaaslam <farehaaslam57@gmail.com>
2026-03-25 11:20:41 +0530

    added testinomial of alumini

--- data/Testimonial.js
+++ data/Testimonial.js
@@ -6,8 +6,43 @@ import rashid from "../images/Testimonials/rashid.jpg";
 import shazia from "../images/Testimonials/shazia.jpg";
 import shilpi from "../images/Testimonials/shilpi.jpg";
 import sample from "../images/Testimonials/sample.jpeg";
+import lal from "../images/Testimonials/lal.png"; 
+import saud from "../images/Testimonials/saud.jpeg";
+import noor from "../images/Testimonials/noor.jpg";
+import zohair from "../images/Testimonials/zohair.jpg";
+import faizan from "../images/Testimonials/faizan.jpeg";
 
 export const testimonials = [
+  {
+    name: "Noor E Ain",
+    img: noor,
+    batch: "B.Tech. 2025 Batch",
+    text:"I graduated in 2025 from the Department of Computer Engineering department at Jamia Millia Islamia. I currently work as a Mobile Application Developer at FAM (YC’19).My time at JMI was truly enriching. The supportive faculty, collaborative environment, and strong technical foundation helped me develop both professionally and personally. Key subjects like DBMS, OOPS, and Data Mining provided a strong foundation that still helps me in my job.Being part of various tech societies gave me valuable experience in teamwork, leadership, networking, and new technologies. This has shaped both my technical and interpersonal skills. I am thankful to the department for playing such an important role in my growth."
+  },
+   {
+    name: "Saud Anwar",
+    img: saud,
+    batch: "B.Tech. 2025 Batch",
+    text:"My journey at Jamia Millia Islamia has been a beautiful blend of learning, growth, and unforgettable memories. It all began online during the first year, where I started exploring Android development while attending virtual classes, making new friends, joining clubs, and enjoying countless online interactions and events. During this time, I secured my first two-month online internship in Android development, which gave me practical exposure early on. When we finally met offline in the third semester, college life truly came alive with friendships and experiences, along with another four-month remote Android internship. In the fourth semester, I decided to strengthen my problem-solving skills through DSA, realizing the importance of balancing enjoyment with career growth. Although I briefly shifted focus toward improving my CGPA, starting competitive programming on Codeforces in the fifth semester turned out to be one of the best decisions of my college life, significantly boosting my problem-solving abilities. After facing rejection in my search for a summer internship, I stayed persistent and applied for winter roles. Fortunately, I received an opportunity from HashiCorp, cleared all interview rounds, and secured a winter internship. That experience was the highlight of my college journey, offering the perfect mix of learning and enjoyment, and it eventually led to a full-time offer with a 17+ LPA package. I am truly grateful to the Computer Engineering department for shaping my technical foundation, resilience, and confidence, which helped me turn aspirations into reality."
+  },
+   {
+    name: "Faizan Choudhary",
+    img: faizan,
+    batch: "B.Tech. 2024 Batch",
+    text:"My four years at the Department of Computer Engineering, Jamia Millia Islamia, were defined by a robust curriculum that prioritized a deep understanding of core engineering principles over fleeting industry trends. This theoretical depth has been invaluable; it provided me with the technical resilience to navigate my current professional role, while simultaneously building the rigorous analytical foundation required for advanced specialization and higher academic pursuits.The program strikes a pragmatic balance—it functions as a high-level training ground for the industry, but its emphasis on fundamental research and complex problem-solving makes it an ideal springboard for those looking to bridge the gap between a B.Tech and a Master’s degree. I am grateful for the faculty's commitment to maintaining an environment that is as academically demanding as it is professionally rewarding."
+  },
+   {
+    name: "Mohammad Zohair",
+    img: zohair,
+    batch: "B.Tech. 2024 Batch",
+    text:"I graduated in 2024 with a Bachelor’s degree from the Department of Computer Engineering, JMI. My journey at Jamia was a steady process of learning and growth, shaped by a mix of varied academics, practical exposure, and constant problem-solving—whether it was tackling technical challenges or navigating everyday situations that pushed me to think better and adapt faster. The coursework helped me develop logical thinking and write cleaner, more efficient code, while labs, projects, and assignments made it easier to connect theory with real-world use. What made the experience truly meaningful was the environment itself—the guidance from faculty, the diversity of perspectives among peers, and a culture that encouraged open conversations, collaboration, and independent thinking. The challenges I faced along the way taught me discipline, adaptability, and confidence. Looking back, I realize that Jamia did more than strengthen my technical foundation; it shaped how I approach learning and growth, becoming an important part of who I am today."
+  },
+  {
+    name: "Lal Pandey",
+    img: lal,
+    batch: "B.Tech. 2024 Batch",
+    text:"My time at Jamia Millia Islamia has been more than just earning a degree. The curriculum gave me a strong academic foundation, but what truly stood out was the people. Seniors were always ready to guide, classmates were collaborative rather than competitive, and professors were approachable and supportive whenever I needed clarity. Being part of student-run societies also helped me step outside academics, build confidence, and grow in ways I didn’t expect. It’s an environment that genuinely supports both learning and personal development."
+  },
   {
     name: "Minhaj Iqbal",
     img: minhaj,
